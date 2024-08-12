0b46509472a407e07c6c9fa1c336f8f128b09e9d
Ulugbek Eshnazarov (reactnenja) <reactnenja@gmail.com>
2024-08-12 05:37:24 +0100

    succes edit  cookiebanner

--- src/components/utils/telegrambuttons/index.jsx
+++ src/components/utils/telegrambuttons/index.jsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
+import { Link } from "lucide-react";
 import React from "react";
 import { BsTelegram } from "react-icons/bs";
 
 const TelegramButton = () => {
     return (
         <motion.div
-            className="fixed bottom-12 right-4 flex justify-end border-2 border-zinc-400/25 z-50 space-x-2 p-2 px-11 bg-white rounded-full shadow-lg animate-pulse"
+            className="fixed bottom-12 right-4 flex justify-end  items-center border-2 border-zinc-400/25 z-50 space-x-2 p-2 px-11 bg-white rounded-full shadow-lg animate-pulse"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
@@ -14,14 +15,14 @@ const TelegramButton = () => {
             <div className="text-sm text-gray-700  font-medium">
                 Bizga yozing!
             </div>
-            <a
-                href="https://t.me/your_telegram_link"
+            <Link
+                to="https://t.me/ulugbekeshnazarovuz"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center justify-center w-10 h-10  bg-blue-500 rounded-full text-white shadow-lg"
             >
-                <BsTelegram className="text-3xl" />
-            </a>
+                <BsTelegram className="text-2xl" />
+            </Link>
         </motion.div>
     );
 };
