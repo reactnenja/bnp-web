c3eda3184e3d03062f3001be03da496eba378226
Ulugbek Eshnazarov (reactnenja) <reactnenja@gmail.com>
2024-08-12 07:18:10 +0100

    success

--- src/components/utils/telegrambuttons/index.jsx
+++ src/components/utils/telegrambuttons/index.jsx
@@ -1,28 +1,27 @@
 import { motion } from "framer-motion";
-import { Link } from "lucide-react";
+import { Send } from "lucide-react";
 import React from "react";
-import { BsTelegram } from "react-icons/bs";
 
 const TelegramButton = () => {
     return (
         <motion.div
-            className="fixed bottom-12 right-4 flex justify-end  items-center border-2 border-zinc-400/25 z-50 space-x-2 p-2 px-11 bg-white rounded-full shadow-lg animate-pulse"
+            className="fixed bottom-12 right-4 flex justify-end items-center border-2 border-zinc-400/25 z-50 space-x-2 p-2 px-11 bg-white rounded-full shadow-lg animate-pulse"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
             whileHover={{ scale: 1.1 }}
         >
-            <div className="text-xl text-gray-700  font-medium">
-                <span> Bizga yozing!</span>
+            <div className="text-xl text-gray-700 font-medium">
+                <span>Bizga yozing!</span>
             </div>
-            <Link
-                to="https://t.me/ulugbekeshnazarovuz"
+            <a
+                href="https://t.me/ulugbekeshnazarovuz"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center justify-center w-10 h-10  bg-blue-500 rounded-full text-white shadow-lg"
+                className="flex items-center justify-center w-14 h-14 rounded-full text-black shadow-lg border"
             >
-                <BsTelegram className="text-3xl" />
-            </Link>
+                <Send className="w-8 h-8" />
+            </a>
         </motion.div>
     );
 };
